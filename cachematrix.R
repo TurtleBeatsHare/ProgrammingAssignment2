c1ad126aced490bc62c918252bce645e7d0991de
TurtleBeatsHare <TurtleBeatsHare@users.noreply.github.com>
2015-05-16 23:45:31 -0600

    Final Version
    Final Version

--- cachematrix.R
+++ cachematrix.R
@@ -19,7 +19,6 @@ makeCacheMatrix <- function(x = matrix()){
     list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
 #Checks to see if Inverse is cached, if true returns cached value.
 #Else computes inverse and caches it.
 cacheSolve <- function(x, ...) {
